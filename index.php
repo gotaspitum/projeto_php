51f5d0f2496eb5e2fa340febe1adb2e4304e3914
gotaspitum <wendsonlima1000@gmail.com>
2025-05-09 20:20:59 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-
+<!DOCTYPE html>>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dr. Tec</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" media="screen"/>
+    <head>
+    <body>
+                <main> 
+        <footer>
+        </footer>
+                </main>
+    </body>
+</html>
